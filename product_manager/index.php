5e586bdb70f57ad744ccdf3287d30fd328311a4c
AakashKarode <ark56@njit.edu>
2017-03-05 22:20:58 -0500

    added category list

--- product_manager/index.php
+++ product_manager/index.php
@@ -21,6 +21,8 @@ if ($action == 'list_products') {
     $categories = get_categories();
     $products = get_products_by_category($category_id);
     include('product_list.php');
+} else if ($action == 'list_categories') {
+            include('category_list.php');
 } else if ($action == 'delete_product') {
     $product_id = filter_input(INPUT_POST, 'product_id', 
             FILTER_VALIDATE_INT);
