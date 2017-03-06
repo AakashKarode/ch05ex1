c666229ea99b345d39aeb0181ec7def47b2a6d04
AakashKarode <ark56@njit.edu>
2017-03-05 22:43:53 -0500

    displays name and delete option

--- product_manager/category_list.php
+++ product_manager/category_list.php
@@ -8,6 +8,17 @@
             <th>&nbsp;</th>
         </tr>        
         <!-- add category rows here -->
+         <?php foreach ($categories as $category) : ?>
+        <tr>
+          <td><?php echo $category['categoryID']; ?></td>
+          <td><?php echo $category['categoryName']; ?></td>
+          <td><form action="delete_category.php" method="post">
+              <input type="hidden" name="category_id"
+                      value="<?php echo $category['categoryID']; ?>">
+              <input type="submit" value="Delete">
+          </form></td>  
+          <?php endforeach; ?>  
+        </tr>
     </table>
 
     <h2>Add Category</h2>
